b6b5df999defa5201c526d1604f2b0761bae1bb8
Rena Banka <renabanka@Renas-MacBook-Pro.local>
2016-12-06 13:39:58 -0600

    beforedeploy

--- migration.sql
+++ migration.sql
@@ -1,6 +1,18 @@
 create database RSVP;
 
-n
+
+
+   create table users (
+       id int not null auto_increment,
+       image LONGTEXT,
+       name varchar(255) not null,
+       email varchar(255) not null,
+       password_hash varchar(61) not null,
+       primary key (id)
+   );
+
+
+
 
 create table events (
     id int not null auto_increment,
@@ -20,9 +32,9 @@ create table events (
 create table event_attendance (
     id int not null auto_increment,
     evt_id int not null references events(id),
---    evt_name not null references events(event_name),
+
     user_id int not null references users(id),
---    user_name not null references users(name),
+
     status VARCHAR(255) not null,
     comments VARCHAR(255),
     event_name VARCHAR(255),
